e1016476fc35b37f88c173050ac40c246a21ba49
sphennequin <sphennequin@gmail.com>
2022-08-29 23:32:40 +0200

    Ajout des fichiers ressources css et js - Appel dans le fichier functions

--- wp-content/themes/easy/functions.php
+++ wp-content/themes/easy/functions.php
@@ -1,4 +1,15 @@
 <?php
 
-# Add document title tag to html head
-add_theme_support( 'title-tag' );
+function easy_supports() {
+    # Add document title tag to html head
+    add_theme_support( 'title-tag' );
+}
+
+function easy_register_assets() {
+    #Add Bootstrap files
+    wp_register_style( 'bootstrap',  get_template_directory_uri() . '/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrap' );
+}
+
+add_action( 'after_setup_theme', 'easy_supports' );
+add_action( 'wp_enqueue_scripts', 'easy_register_assets' );
