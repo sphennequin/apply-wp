168d0915e816d311e7ba1f944246cbd0bc8620f4
sphennequin <sphennequin@gmail.com>
2022-08-29 23:01:10 +0200

    Ajout fichier footer.php et appel du footer dans index.php

--- wp-content/themes/easy/index.php
+++ wp-content/themes/easy/index.php
@@ -1,6 +1,6 @@
 
-	<?php get_header() ?>
-	
+<?php get_header() ?>
+
 	<!-- main -->
 	<div id="main">
 		<!-- header -->
@@ -201,49 +201,4 @@
 			</div>
 		</div>
 
-		<!-- footer -->
-		<footer>
-			<div class="container">
-				<div class="row">
-
-					<div class="footer-wrapper">
-						
-						<div class="footer-info col-sm-4 col-xs-12">
-							<ul>
-								<li><a href="tel:+9145632973" class="white-link"><i class="fa fa-phone-square"></i>+9145632973</a></li>
-								<li><a href="mailto:work@jamesvincent.me" class="white-link"><i class="fa fa-google"></i>work@jamesvincent.me</a></li>
-							</ul>
-						</div>
-
-						<div class="footer-name col-sm-4 col-xs-12">
-							<span>James Vincent</span>
-							<div class="footer-address">
-								James Vincent, D12/3 <br>
-								Kharkov, Ukraine
-							</div>
-						</div>
-
-						<div class="footer-socials col-sm-4 col-xs-12">
-							<ul class="socials">
-								<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
-								<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
-								<li><a href="#"><i class="fa fa-behance-square"></i></a></li>
-								<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
-							</ul>
-						</div>
-
-					</div>
-
-				</div>
-			</div>
-		</footer>
-	</div>
-	
-	<!-- javascript -->
-	<script src="js/plugins.js"></script>
-	<script src="js/smooth-scroll.js"></script>
-	<script src="js/jquery.countTo.js"></script>
-	<script src="js/particles.min.js"></script>
-	<script src="js/main.js"></script>
-</body>
-</html>
+<?php get_footer() ?>	
